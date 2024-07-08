f8fccfe30a04d6b7710f673bfcc9b2c0bdfb0297
AmandaIreri <amandairerii@gmail.com>
2024-07-08 08:50:32 +0300

    first commit

---
+++ app/src/main/java/com/amandai/enwallet/Expense.kt
@@ -0,0 +1,9 @@
+package com.amandai.enwallet
+
+import java.time.temporal.TemporalAmount
+
+data class Expense(
+    var name: String,
+    var amount: Int,
+    var date: String
+)
---
+++ app/src/main/java/com/amandai/enwallet/ExpenseAdapter.kt
@@ -0,0 +1,2 @@
+package com.amandai.enwallet
+
